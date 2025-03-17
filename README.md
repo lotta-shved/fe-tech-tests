95eb98916579e2fdeacf3ff1024edf572fc7b9a1
Lotta <lotta.shved@incredible-web.com>
2025-03-17 18:24:56 +0100

    removed user details and added to live-coding task

--- README.md
+++ README.md
@@ -11,8 +11,3 @@ Run `npm i` on your machine to install all the necessary dependencies
 
 # Running in developer mode
 Use `npm run dev` to have the project running locally 
-
-# Tasks to warm up before the interview:
-- Implement a striped background for the table rows on the `/users` page.
-- Use `useAddOneUserMutation` to "add" a user to the DB. NB: https://dummyjson.com/ service is used and no data will actually be added, just make sure that you get a successful response
-- Populate User's Details page (`/users/:id`), using data from `https://dummyjson.com/users/{{id}}`
--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -1,8 +1,11 @@
 import { LimitsTable } from "@/components/limits-table";
-import { Avatar, Box, Card, CardContent, CardHeader, List, ListItem, ListItemAvatar, ListItemText, Stack, Typography } from "@mui/material";
+import { Card, CardContent, CardHeader, Stack, Typography } from "@mui/material";
 import { useParams } from "react-router-dom";
-import PersonIcon from '@mui/icons-material/Person';
 
+/**
+ * api to get one user https://dummyjson.com/users/{{id}}
+ * @returns User Details Page
+ */
 export const UserPage = () => {
   const { userId } = useParams();
 
@@ -15,35 +18,7 @@ export const UserPage = () => {
       <Typography variant="h4">User Details: {userId}</Typography>
       <Card sx={{ maxWidth: 680, }}>
         <CardContent>
-          <Stack direction="row" spacing={1}>
-            <Box style={{ marginTop: '1rem' }}>
-              <Avatar>
-                <PersonIcon />
-              </Avatar>
-            </Box>
-            <List dense>
-              <ListItem>
-                <Typography>Name Surname</Typography>
-              </ListItem>
-              <ListItem>
-                <Typography>Email: email</Typography>
-              </ListItem>
-              <ListItem>
-                <Typography>Username: username</Typography>
-              </ListItem>
-            </List>
-            <List dense>
-              <ListItem>
-                <Typography>Age: 30</Typography>
-              </ListItem>
-              <ListItem>
-                <Typography>Currency: EUR</Typography>
-              </ListItem>
-              <ListItem>
-                <Typography>Address: City, Postal Code, Country</Typography>
-              </ListItem>
-            </List>
-          </Stack>
+          User Information: name, email, address, etc. Also include an Avatar with some generic icon, ex, PersonIcon
         </CardContent>
       </Card>
       <Card>
